3650b990bca653753a7a5088f9ab31641eead041
roger1547 <roger_arnez_22@hotmail.com>
2022-11-27 23:54:01 -0300

    terminacion de tp

--- web/login.js
+++ web/login.js
@@ -1,17 +1,20 @@
-function validate()
-{
-var username=document.getElementById("username").value;
-var password=document.getElementById("password").value;
-if(username=="admin"&& password=="lala")
-{
-   alert('login correcto');
-    return false;
+function validate(){
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+    if (username ="juan" && password =="1234") {
+        window.location.href = "home.html"; 
+        return false;
+     } else{
+        alert("usuario o password incorrecto");
+     }
 }
-else
-{
-    alert("El usuario no existe");
-}
-}
+
+let button = document.getElementById("button");
+
+button.addEventListener("click", (e)=> {
+    e.preventDefault();
+    //validate();
+})
 function finalizar(){
     alert ("COMPRASTE EL MENU HAMBURGUESA CON QUESO DESEA COMPRAR OTRO")
 }
